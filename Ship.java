92924dc1b84a1caf895a4e68229c24fa19ea35be
ricoreid <reid.ricardo@gmail.com>
2020-02-22 02:40:46 +0100

    Class diagrams and implementation of Square and Ship functions - not complete

---
+++ Ship.java
@@ -0,0 +1,62 @@
+public class Ship
+{
+    private enum ShipType {
+                    CARRIER,
+                    BATTLESHIP,
+                    CRUISER,
+                    SUBMARINE,
+                    DESTROYER
+                }
+    private enum ShipStatus {
+                    OPERATIONAL,
+                    DESTROYED
+                }
+    ShipType myShipType;
+    ShipStatus myStipStatus;
+    private Square location;
+    private int shipSize;
+
+    public Ship(ShipType myShipType, Square location, ShipStatus myShipStatus)
+    {
+        this.myShipType = myShipType;
+        this.location = location;
+        this.myStipStatus = myShipStatus;
+
+        switch(myShipType)
+        {
+            case CARRIER:
+                this.shipSize = 1;
+                break;
+            case BATTLESHIP:
+                this.shipSize = 1;
+                break;
+            case CRUISER:
+                this.shipSize = 1;
+                break;
+            case SUBMARINE:
+                this.shipSize = 1;
+                break;
+            case DESTROYER:
+                this.shipSize = 1;
+                break;
+            default:
+                System.out.println("Invalid input!");
+
+        } // end switch
+
+    } // end constructor
+
+    public void setShipSize(int size)
+    {
+        
+        this.shipSize = size; // or maybe i should set this to 'this.shipSize - 1'
+
+    } // end getShipSize()
+
+    public int getShipSize()
+    {
+        return this.shipSize;
+        
+    } // end setShipSize()
+
+} // end class Ship
--- Square.java
+++ Square.java
@@ -3,6 +3,22 @@ import java.util.ArrayList;
 public class Square
 {
     private ArrayList<ArrayList<String>> square = new ArrayList<ArrayList<String>>();
+    private int length; // not necessary
+    private int width; // not necessary
+
+    public Square(int length, int width)
+    {
+        this.length = length;
+        this.width = width;
+
+        for(int row = 0; row < length; row++)
+        {
+            for(int column = 0; column < width; column++)
+            {
+                square.get(row).add(" - ");
+            }
+        }
+    }
 
 
     public ArrayList<ArrayList<String>> getSquare()
